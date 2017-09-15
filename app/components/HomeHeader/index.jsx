3f70a202393fdcaae99fbbb5c9e6d77c43a3b3be
Jason <jason.zhour@gmail.com>
2017-09-15 10:48:31 +0800

    init

--- app/components/HomeHeader/index.jsx
+++ app/components/HomeHeader/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import PureRenderMixin from 'react-addons-pure-render-mixin'
+import { Link, hashHistory } from 'react-router'
+
+import SearchInput from '../SearchInput'
 
 import './style.less'
 
@@ -12,21 +15,29 @@ class HomeHeader extends React.Component {
         return (
             <div id="home-header" className="clear-fix">
                 <div className="home-header-left float-left">
-                    <span>{this.props.cityName}</span>
-                    &nbsp;
-                    <i className="icon-angle-down"></i>
+                    <Link to="/city">
+                        <span>{this.props.cityName}</span>
+                        &nbsp;
+                        <i className="icon-angle-down"></i>
+                    </Link>
                 </div>
                 <div className="home-header-right float-right">
-                    <i className="icon-user"></i>
+                    <Link to="/Login">
+                        <i className="icon-user"></i>
+                    </Link>
                 </div>
                 <div className="home-header-middle">
                     <div className="search-container">
                         <i className="icon-search"></i>
-                        <input type="text" placeholder="请输入关键字"/>
+                        &nbsp;
+                        <SearchInput value="" enterHandle={this.enterHandle.bind(this)}/>
                     </div>
                 </div>
             </div>
         )
+    }
+    enterHandle(value) {
+        hashHistory.push('/search/all/' + encodeURIComponent(value))
     }
 }
 
